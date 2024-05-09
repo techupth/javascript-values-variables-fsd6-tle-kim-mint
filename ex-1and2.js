4dee72e6fcc0a9fc2c21c4f631b9d2424232b1f0
PNKim <paljapon.nait@gmail.com>
2024-05-09 12:52:02 +0700

    feat:ex2

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,3 +1,5 @@
+// Exercise #1: Variable Creation
+// Start coding here
 let name = "Karter";
 let surName = "Anderson";
 let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
@@ -8,3 +10,6 @@ const restaurantCount = 1500;
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+totalPrice = 5000;
+restaurantCount = 2500;
