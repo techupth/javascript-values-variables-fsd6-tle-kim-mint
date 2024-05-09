c40289c9f1edee634bbbc2003c6af2098c161a5a
MINT <mint.2978@gmail.com>
2024-05-09 14:08:36 +0700

    feat:ex1-3

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,15 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let firstName = "karter";
+let surName = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+const nameRestaurant = "Chef's Table by Chef James";
+const checkoutBill = 15000;
+const totalOpenNowRestaurant = 1500;
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+checkoutBill = 5000;
+totalOpenNowRestaurant = 2500;
+console.log(totalOpenNowRestaurant);
