63f519dad889789e921139e05bd333ca1ce5457c
tle <tlemakuz@gmail.com>
2024-05-09 15:22:40 +0700

    feat:ex1-4

--- ex-3.js
+++ ex-3.js
@@ -1,6 +1,7 @@
 // Exercise #3: Celsius to Fahrenheit
 let celsius = 30;
+let Fahrenheit = (celsius * 1.8)/32;
+let result = `${Fahrenheit} Fahrenheit`;  
+console.log(result);
 
-// Start coding here;
-
-let fahrenheit;
+                                        
