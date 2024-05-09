6951b402e2f653bdff3750db091cd1fec6b3fd5a
PNKim <paljapon.nait@gmail.com>
2024-05-09 12:49:02 +0700

    feat: ex1

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,10 @@
-//Exercise #1: Variable Creation
-//Start coding here
+let name = "Karter";
+let surName = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+
+const restaurantName = "Chef's Table by Chef James";
+const totalPrice = 15000;
+const restaurantCount = 1500;
 
 //Exercise #2: Variable Reassignment
 //Start coding here
